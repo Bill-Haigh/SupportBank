8bc5b30d75c239dc56d92a9889ac85d48b7c2eb0
Bill Haigh <bill.haigh@softwire.com>
2025-07-17 08:49:02 +0100

    FIX: converted Transactions2014 to a reusable list

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import csv
 #this opens the csv file and then maps each line to a dictionary with the column headers as the keys.
 with open ('./DataFiles/Transactions2014.csv', mode = 'r') as file:
     Transactions2014 = csv.DictReader(file)
+    Transactions2014List = list(Transactions2014)
     accountsList = []
     #loops through transactions adding all names to a list of accounts
-    for lines in Transactions2014:
+    for lines in Transactions2014List:
        accountsList.append(lines['From'])
        accountsList.append(lines['To'])
     #removes duplicates
@@ -14,11 +15,7 @@ with open ('./DataFiles/Transactions2014.csv', mode = 'r') as file:
     for account in accountsList:
         accountsDict[account] = 0
 
-#i am unsure why it requires me to open a with block again
-#adds the transactions to the accounts in the dictionary
-with open ('./DataFiles/Transactions2014.csv', mode = 'r') as file:
-    Transactions2014 = csv.DictReader(file)
-    for lines in Transactions2014:
+    for lines in Transactions2014List:
         accountsDict[lines['From']] -= float(lines['Amount'])
         accountsDict[lines['To']] += float(lines['Amount'])
 

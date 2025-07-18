39cc1dc6e56bc535a3a0fa4ef04d00fd2f85134c
Bill Haigh <bill.haigh@softwire.com>
2025-07-18 09:12:36 +0100

    FEAT/FIX: you can now use arguements in the command line to run list_all for the file you want

--- supportbank/main.py
+++ supportbank/main.py
@@ -1,59 +1,49 @@
+#! I am also aware that I have used a mixture of camel/snake/etc will tidy up at the end. Also why doesnt python have multi-line comments
+# TODO change to classes "accounts" and "transactions"
+#! Now in the console you can run "poetry run List_All Transactions2014" or other files as you like
 
-#! I am aware that I havent quite got there with making the scripts run correctly. WIP.
-#! I am also aware that I have used a mixture of camel/snake/etc will tidy up at the end.
-#TODO change to classes "accounts" and "transactions"
-#TODO is there a way of passing parameters to entry points on console
 import csv
 import logging
+import sys
+
 logger = logging.getLogger()
-logging.basicConfig(filename='SupportBank.log', filemode='w', level=logging.DEBUG)
-logger.debug('I am in the log file')
+logging.basicConfig(filename="SupportBank.log", filemode="w", level=logging.DEBUG)
+logger.debug("I am in the log file")
 
-with open ('./DataFiles/Transactions2014.csv', mode = 'r') as file:
-    Transactions2014 = csv.DictReader(file)
-    Transactions2014List = list(Transactions2014)
-    logger.info('Transactions2014 read and transoformed to list')
-
-with open ('./DataFiles/DodgyTransactions2015.csv', mode = 'r') as file:
-    Transactions2015 = csv.DictReader(file)
-    Transactions2015List = list(Transactions2015)
-    logger.info('Transactions2015 read and transoformed to list')
+def read_transactions(filename):
+    with open(filename, mode='r') as file:
+        reader = csv.DictReader(file)
+        transactions = list(reader)
+        logger.info(f'{filename} read and transformed to list')
+        return transactions
 
 def List_All(transactions):
     accountsList = []
     for lines in transactions:
-        accountsList.append(lines['From'])
-        accountsList.append(lines['To'])
+        accountsList.append(lines["From"])
+        accountsList.append(lines["To"])
     accountsList = list(set(accountsList))
     accountsDict = {}
     for account in accountsList:
         accountsDict[account] = 0
 
-    for lines in Transactions2014List:
-        accountsDict[lines['From']] -= int(float(lines['Amount'])*100)
-        accountsDict[lines['To']] += int(float(lines['Amount'])*100)
+    for lines in transactions:
+        accountsDict[lines["From"]] -= int(float(lines["Amount"]) * 100)
+        accountsDict[lines["To"]] += int(float(lines["Amount"]) * 100)
     print(accountsList)
     print(accountsDict)
+
+def cli_List_All():
+    if len(sys.argv) < 2:
+        print("Usage: poetry run List_All <csv_filename>")
+        sys.exit(1)
+    filename = f"./DataFiles/{sys.argv[1]}.csv"
+    transactions = read_transactions(filename)
+    List_All(transactions)
 
 def List(account, transactions):
     listOfTransactions = []
     for lines in transactions:
-        if lines['From'] == account or lines['To'] == account:
+        if lines["From"] == account or lines["To"] == account:
             listOfTransactions.append(lines)
             print(lines)
-    #print(listOfTransactions)
-
-
-def Test():
-    print("that worked")
-
-
-
-
-#List_All(Transactions2014List)
-#List('Rob S', Transactions2014List)
-List_All(Transactions2015List)
-List('Sarah T', Transactions2015List)
-
-
-

d2afd251473301491b355dabc5db4008b6a33050
Bill Haigh <bill.haigh@softwire.com>
2025-07-18 10:33:36 +0100

    FEAT/REFACTOR: changed to classes

--- supportbank/main.py
+++ supportbank/main.py
@@ -10,28 +10,49 @@ logger = logging.getLogger()
 logging.basicConfig(filename="SupportBank.log", filemode="w", level=logging.DEBUG)
 logger.debug("I am in the log file")
 
+class Transaction:
+    def __init__(self, data):
+        self.Date = data["Date"]
+        self.From = data["From"]
+        self.To = data["To"]
+        self.Narrative = data["Narrative"]
+        self.Amount = float(data["Amount"])
+
+class Account:
+    def __init__(self, name):
+        self.name = name
+        self.balance = 0
+        self.transactions = []
+
+    def apply_transaction(self, transaction):
+        if transaction.From == self.name:
+            self.balance -= int(transaction.Amount * 100)
+        if transaction.To == self.name:
+            self.balance += int(transaction.Amount * 100)
+        self.transactions.append(transaction)
+
 def read_transactions(filename):
     with open(filename, mode='r') as file:
         reader = csv.DictReader(file)
-        transactions = list(reader)
+        transactions = [Transaction(row) for row in reader]
         logger.info(f'{filename} read and transformed to list')
         return transactions
 
 def List_All(transactions):
-    accountsList = []
-    for lines in transactions:
-        accountsList.append(lines["From"])
-        accountsList.append(lines["To"])
-    accountsList = list(set(accountsList))
     accountsDict = {}
-    for account in accountsList:
-        accountsDict[account] = 0
 
-    for lines in transactions:
-        accountsDict[lines["From"]] -= int(float(lines["Amount"]) * 100)
-        accountsDict[lines["To"]] += int(float(lines["Amount"]) * 100)
-    print(accountsList)
-    print(accountsDict)
+    for tx in transactions:
+        for name in [tx.From, tx.To]:
+            if name not in accountsDict:
+                accountsDict[name] = Account(name)
+        accountsDict[tx.From].apply_transaction(tx)
+        accountsDict[tx.To].apply_transaction(tx)
+
+    print("Balances:")
+    for account in accountsDict.values():
+        print(f"{account.name}: £{account.balance / 100:.2f}")
+    print("If you wish to see a detailed list of transactions do the following:")
+    print("poetry run list_account <account> <csv_filename>")
 
 def cli_List_All():
     if len(sys.argv) < 2:
@@ -42,8 +63,15 @@ def cli_List_All():
     List_All(transactions)
 
 def List(account, transactions):
-    listOfTransactions = []
-    for lines in transactions:
-        if lines["From"] == account or lines["To"] == account:
-            listOfTransactions.append(lines)
-            print(lines)
+    for tx in transactions:
+        if tx.From == account or tx.To == account:
+            print(f"{tx.Date} | From: {tx.From} | To: {tx.To} | {tx.Narrative} | £{tx.Amount:.2f}")
+
+def cli_List():
+    if len(sys.argv) < 3:
+        print("Usage: poetry run list_account <account> <csv_filename>")
+        sys.exit(1)
+    account = sys.argv[1]
+    filename = f"./DataFiles/{sys.argv[2]}.csv"
+    transactions = read_transactions(filename)
+    List(account, transactions)
